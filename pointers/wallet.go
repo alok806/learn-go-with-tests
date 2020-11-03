7d5f60656810e1548556083aef56be10f4f0056a
alok806 <alok806@gmail.com>
2020-11-03 14:53:49 -0800

    revisiting - first edit

---
+++ pointers/wallet.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+/*
+Stringer for Bitcoin
+*/
+type Stringer interface {
+	String() string
+}
+
+/*
+Bitcoin type int
+*/
+type Bitcoin int
+
+/*
+Wallet struct
+*/
+type Wallet struct {
+	balance Bitcoin
+}
+
+/*
+String print for Bitcoin
+*/
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+/*
+Deposit amount to wallet
+*/
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+/*
+Balance check for wallet
+*/
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
