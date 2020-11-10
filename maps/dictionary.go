07eff83392169f912a50e73de3543954827c4ccd
alok806 <alok806@gmail.com>
2020-11-09 23:06:45 -0800

    Refactor and error check

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"errors"
+)
+
+/*
+ErrNotFound message
+*/
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
 /*
 Dictionary map holding strings
 */
@@ -8,6 +17,10 @@ type Dictionary map[string]string
 /*
 Search method for Dictionary
 */
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+	return definition, nil
 }
